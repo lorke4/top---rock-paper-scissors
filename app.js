f60e5d1039e8cf72b18b050272c3dda5ed9d6912
Máté <mate.lorinczi97@gmail.com>
2022-07-30 21:15:37 +0200

    Add game over

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ function computerPlay() {
     return picks[Math.floor(Math.random() * 3)]
 }
 
+const rounds = []
+
 // const playerSelection = () => prompt("Pick an option [Rock, Paper, Scissor]").toString().toLowerCase();
 
 function playRound(playerSelection, computerSelection) {
@@ -45,40 +47,51 @@ function showRoundResult(results) {
 }
 
 function announceGameResults(rounds) {
-    if (rounds.filter(word => word === 'The computer').length == rounds.filter(word => word === 'You').length) {
-        console.log(`The game ended in a draw, you both have ${rounds.filter(word => word === 'The computer').length} points.`)
-    } else if (rounds.filter(word => word === 'The computer').length > rounds.filter(word => word === 'You').length) {
-        console.log(`The computer won the game, with ${rounds.filter(word => word === 'The computer').length} points.`)
+    const scoreBoard = document.querySelector('.scoreboard')
+    const resultsDiv = document.createElement('div');
+    const resultsText = document.createElement('p');
+    resultsDiv.appendChild(resultsText)
+    scoreBoard.appendChild(resultsDiv)
+    if (rounds.filter(word => word === 'The computer').length > rounds.filter(word => word === 'The player').length) {
+       return resultsText.innerText = "The computer won the game."
     } else {
-        console.log(`You won the game, with ${rounds.filter(word => word === 'You').length} points.`)
+       return resultsText.innerText = "You won the game."
+    }
+}
+
+function stopGame() {
+    const buttons = document.querySelectorAll('button')
+    buttons.forEach(e => e.removeEventListener('click', gamePlay));
+}
+
+function checkGameOver(rounds){
+    if((rounds.filter(word => word === 'The computer').length) >= 5 || (rounds.filter(word => word === 'The player').length) >= 5){
+        announceGameResults(rounds);
+        stopGame()
     }
 }
 
 function displayScore(results) {
     const playerScores = document.querySelector('.score-left');
-    let playerWins = parseInt(playerScores.innerText);
-    playerWins += results.filter(word => word === 'The player').length;
+    const playerWins = results.filter(word => word === 'The player').length
     if(playerWins) {
         playerScores.innerText = playerWins.toString();
     }
 
 
     const computerScores = document.querySelector('.score-right')
-    let computerWins = parseInt(computerScores.innerText);
-    computerWins += results.filter(word => word === 'The computer').length
+    const computerWins = results.filter(word => word === 'The computer').length
     if(computerWins) {
         computerScores.innerText = computerWins.toString();
     }
 }
 
-function game() {
-    rounds = []
+function gamePlay() {
     results = playRound(this.innerText.toLowerCase(), computerPlay())
     showRoundResult(results)
     rounds.push(results.winner)
     displayScore(rounds)
-    return rounds
-    // return showGameResults(rounds)
+    checkGameOver(rounds)
 }
 
 
@@ -86,7 +99,7 @@ function game() {
 
 function gameControll(){
     const buttons = document.querySelectorAll('button')
-    buttons.forEach(e => e.addEventListener('click', game));
+    buttons.forEach(e => e.addEventListener('click', gamePlay));
 }
 
-gameControll()
+gameControll();
